0457b61fa8a03798dfb24597ab0f0ce33d733942
najar1412 <rory.jarrel@gmail.com>
2017-11-07 17:04:12 -0500

    about updated

--- flatlandsio/modules/data.py
+++ flatlandsio/modules/data.py
@@ -65,11 +65,11 @@ about = {
         'content': {
             1: {
                 'title': '',
-                'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                'content': "hello! I'm Rory a software developer from sunny England residing in New York. flatlands is considered a portfolio of open source projects as well as a collection of articles that are either original content or shamelessly stolen from others (credited were applicable) and condensed for quick reference. any and all stolen content are from genuine code heroes. i appreciate and learn from them every day."
             },
             2: {
                 'title': '',
-                'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                'content': "as technical director in the field of architecture visualisation. my day-to-day consists of building out our studios pipeline, hacking on proprietary software and developing tooling for our visualisation artists to producing stunning renderings."
             }
         }
     },
@@ -78,15 +78,15 @@ about = {
         'content': {
             1: {
                 'title': 'can i use the software on your site?',
-                'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                'content': "all software on Flatlands is complete with a licence file, that are mainly under the MIT umbrella unless otherwise stated."
             },
             2: {
                 'title': 'where do you host your sites?',
-                'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                'content': "i predominately use digitalocean for most of my hosting with some aws sprinkled in here and there."
             },
             3: {
                 'title': 'whats the best way to contact you for possible work?',
-                'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                'content': "my plate is pretty full currently, but room can always be made for interesting projects. the best place to enquire would be [here](mailto:)."
             },
         }
     },
